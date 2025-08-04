ff3bc021c9bb061ff6d28e485444500e0e687369
eahubgit32 <eagithub32@gmail.com>
2025-08-04 08:34:32 -0400

    Add files via upload

---
+++ Print first 10 natural numbers using while loop.py
@@ -0,0 +1,21 @@
+# Print first 10 natural numbers using loops
+
+# Using while loop
+print ("Using while loop")
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+# Using for loop
+print ("Using for Loop")
+for x in range (1, 11):
+    print(x)
+    x+1
+
+
+# Another User Case for loop
+print ("Another User case for loop")
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for number in numbers:
+    print(number)
